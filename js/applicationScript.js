59df7a11694314031ed23233e0d48730fac55f79
CAEUser <mail@rwth>
2021-09-21 16:00:48 +0000

    ApplicationScript edited by Anonymous

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -40,7 +40,7 @@ var init = function() {
 
   };
 
-  client = new Las2peerWidgetLibrary("$Microservice_Url$/"music"", iwcCallback, '*');
+  client = new Las2peerWidgetLibrary("$Microservice_Url$/"music""", iwcCallback, '*');
 
 
 
